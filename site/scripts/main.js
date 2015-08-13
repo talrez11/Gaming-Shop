890b93e22797bcd1874a6cb8a43bece00492b442
talrez11 <tal@way2cy.com>
2015-08-13 18:48:51 +0300

    Added shop cart item function

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -45,10 +45,113 @@ Site.is_mobile = function() {
 	return result;
 };
 
+Site.ItemView = function(item) {
+	var self = this;
+
+	self.item = item;
+	self.cart = item.cart;
+	self.currency = null;
+	self.exchange_rate = 1;
+
+	self.container = null;
+	self.label_name = null;
+	self.label_count = null;
+	self.label_total = null;
+	self.option_remove = null;
+
+	/**
+	 * Complete object initialization.
+	 */
+	self._init = function() {
+		// get list containers
+		var item_list = self.cart.get_list_container();
+		self.currency = self.cart.default_currency;
+
+		// create container
+		self.container = $('<li>').appendTo(item_list);
+		self.container.addClass('item');
+
+		// create labels
+		self.option_remove = $('<a>').appendTo(self.container);
+		self.option_remove
+				.attr('href', 'javascript: void(0);')
+				.on('click', self._handle_remove)
+				.html(language_handler.getText('shop', 'remove'));
+
+		self.figure = $('<figure>').appendTo(self.container);
+		self.image = $('<img>').appendTo(self.figure);
+		self.label_total = $('<span>').appendTo(self.container);
+		self.label_total
+				.addClass('total')
+				.attr('data-currency', self.currency);
+		self.label_count = $('<span>').appendTo(self.container);
+		self.label_count.addClass('count');
+		self.label_name = $('<span>').appendTo(self.container);
+		self.label_name.addClass('name');
+		// create options
+
+	};
+
+	/**
+	 * Handle clicking on remove item.
+	 *
+	 * @param object event
+	 */
+	self._handle_remove = function(event) {
+		event.preventDefault();
+		self.item.remove();
+	};
+
+	/**
+	 * Handler externally called when item count has changed.
+	 */
+	self.handle_change = function() {
+		self.image
+				.attr('src', self.item.image)
+				.attr('alt', self.item.name[language_handler.current_language])
+		self.label_name.text(self.item.name[language_handler.current_language]);
+		self.label_count.text(self.item.count);
+		self.label_total
+				.text((self.item.count * self.item.price * self.exchange_rate).toFixed(2))
+				.attr('data-currency', self.currency);
+	};
+
+	/**
+	 * Handle shopping cart currency change.
+	 *
+	 * @param string currency
+	 * @param float rate
+	 */
+	self.handle_currency_change = function(currency, rate) {
+		// store values
+		self.currency = currency;
+		self.exchange_rate = rate;
+
+		// update labels
+		self.handle_change();
+	};
+
+	/**
+	 * Handler externally called before item removal.
+	 */
+	self.handle_remove = function() {
+		self.container.remove();
+	};
+
+	// finalize object
+	self._init();
+}
+
 /**
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
+
+	Site.cart = new Caracal.Shop.Cart();
+	Site.cart
+			.ui.add_item_list($('div#popup ul'))
+			.add_item_view(Site.ItemView);
+
 	advertise = new PageControl('div.link_images_container','a')
 	advertise.attachControls($('div.btn_controls a'))
 	.setInterval(6000)
@@ -72,6 +175,15 @@ Site.on_load = function() {
 		cart.toggleClass('open');
 	});	
 
+	/*Function inserting item to cart*/
+	function insertToCart() {
+		var uid = $('div.product_wrap').data('id');
+		Site.cart.add_item_by_uid(uid);
+		cart.addClass('open');
+	}
+
+	var btn_add_cart = $('a.add_cart');
+	btn_add_cart.on('click',insertToCart);
 };
 
 
